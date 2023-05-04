391df3134f052d1f38b1d630c275be176e8437bc
Alexander Löhr <alex@alexloehr.net>
2023-05-04 13:00:24 +0200

    use latest vue and vue-router version

--- lib.js
+++ lib.js
@@ -1,16 +1,33 @@
 import './public/stylesheets/style.styl'
 
-import Vue from "vue"
-import router from 'vue-router'
-import App from './vue/pages/app'
-import myRouter from './vue/lib/myRouter'
+import {createApp} from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
 
+import App from './vue/pages/app'
+import Upload from "./vue/pages/upload.vue"
+import FileList from "./vue/pages/fileList.vue"
+import Media from "./vue/pages/media.vue"
+import MediaItem from "./vue/pages/mediaItem.vue"
+import Stats from "./vue/pages/stats.vue"
 
 /////////////////////////////////////////////////////////////////////////
 
-Vue.use(router)
-new Vue({
-    router: myRouter,
-    render: h => h(App)
-}).$mount('#vue')
+const router = createRouter({
+   history: createWebHistory(),
+   routes: [
+      {path: '/', redirect: "/upload"},
+      {path: '/upload', component: Upload},
+      {path: '/file-list', component: FileList},
+      {path: '/media', component: Media},
+      {path: '/media/:id', component: MediaItem},
+      {path: '/stats', component: Stats},
+   ]
+})
+
+/////////////////////////////////////////////////////////////////////////
+
+
+const app = createApp(App)
+app.use(router)
+app.mount('#vue')
 
