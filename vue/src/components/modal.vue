5b9a27b57e630fb1778517bc61f1e3752a675e42
Alexander Löhr <alex@alexloehr.net>
2023-05-04 13:00:28 +0200

    adapt style | converting modal.vue to composition api

--- vue/src/components/modal.vue
+++ vue/src/components/modal.vue
@@ -1,43 +1,51 @@
 <template>
-    <div class="modal " id="" :class="modalClass">
+    <div class="modal active" id="" v-if="isActive">
         <a href="#close" class="modal-overlay" aria-label="Close" @click="cancel"></a>
         <div class="modal-container">
             <div class="modal-header">
                 <a href="#close" class="modal-x icon-cancel" aria-label="Close" @click="cancel"></a>
-                <h3>{{headerTitle}}</h3>
+                <h3>{{ headerTitle }}</h3>
             </div>
             <div class="modal-body">
                 <slot></slot>
             </div>
             <div class="modal-footer">
-                <button :class="okTitleIcon" @click="ok">{{okTitle}}</button>
-                <button class="icon-cancel" @click="cancel">Cancel</button>
+                <button @click="ok" class="p-1-2">
+                    <span :class="okTitleIcon"></span>
+                    {{ okTitle }}
+                </button>
+                <button class="ms-2 p-1-2 " @click="cancel">
+                    <span class="icon-cancel"></span>Cancel
+                </button>
             </div>
         </div>
     </div>
 </template>
 
-<script>
+<script setup>
 
-    export default {
-        name: "modal",
-        props: ["headerTitle", "isActive", "okTitle", "okTitleIcon"],
-        data() {
-            return {}
-        },
-        computed: {
-            modalClass: function () {
-                return this.isActive ? "active" : "deactive"
-            },
-        },
-        methods: {
-            ok: function () {
-                this.$emit("ok")
-            },
-            cancel: function () {
-                this.$emit("cancel")
-            }
-        },
-    }
+import {computed, defineProps} from "vue"
+
+/////////////////////////////////////////////////////////////////////////
+
+const props = defineProps({
+   headerTitle: String,
+   isActive: Boolean,
+   okTitle: String,
+   okTitleIcon: String,
+})
+
+const emit = defineEmits(["ok", "cancel"])
+
+/////////////////////////////////////////////////////////////////////////
+
+function ok () {
+   emit("ok")
+}
+
+function cancel () {
+   emit("cancel")
+}
+
 </script>
 
