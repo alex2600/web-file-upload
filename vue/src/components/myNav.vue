b9f8541e32fffe52a89057d91fa8c2a9a3d2891e
Alexander Löhr <alex@alexloehr.net>
2023-05-04 13:00:25 +0200

    testing

--- vue/src/components/myNav.vue
+++ vue/src/components/myNav.vue
@@ -9,13 +9,23 @@
             |
             <router-link to="/stats">Stats</router-link>
         </nav>
-
     </div>
+   <!--    <div style="position:absolute; right: 0; top:0;z-index: 8888; background-color:#777;">-->
+   <!--        <a @click.prevent="load">direct link to files</a>-->
+   <!--    </div>-->
 </template>
 
-<script>
-    export default {
-        name: "myNav",
-    }
+<script setup>
+
+// async function load () {
+//    try {
+//       const res = await fetch("http://localhost:3002/api/file/list")
+//       console.log(res)
+//        console.log(await res.headers.values().next())
+//    } catch (ex) {
+//       console.error(ex)
+//    }
+// }
+
 </script>
 
