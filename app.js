2a5d7caa83e34ddec25d5f2e9361e300b4a026d1
Alexander Löhr <alex@alexloehr.net>
2023-05-04 13:46:49 +0200

    cleanup express

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var formidable = require("formidable")
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const formidable = require("formidable")
 
 const settings = require("./settings")
 const cors = require("cors")
@@ -12,65 +12,63 @@ const cron = require("./lib/util/cron").init() // init
 
 /////////////////////////////////////////////////////////////////////////
 
-var app = express();
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 app.set('json spaces', 3)
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger('dev'))
 app.use(cors())
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 // app.use(bodyParser.json({limit: "12GB"})); // not working - still "request entity too large"
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-// app.use(formidable.parse({
-//     uploadDir: settings.uploadDir,
-//     multiples: true
-// }))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use(require('stylus').middleware(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 /////// ROUTES ////////////////////////////////////////////////////////////////
 
-app.use('/api/upload', require("./routes/upload"));
-app.use('/api/file', require("./routes/file"));
-app.use('/api/version', require("./routes/version"));
-app.use('*', require("./routes/index"));
+app.use('/api/upload', require("./routes/upload"))
+app.use('/api/file', require("./routes/file"))
+app.use('/api/version', require("./routes/version"))
+app.use('*', function (req, res, next) {
+   res.status(500).json({status: "error", message: "Invalid route"})
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+   const err = new Error('Not Found')
+   err.status = 404
+   next(err)
+})
 
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+   app.use(function (err, req, res, next) {
+      res.status(err.status || 500)
+      res.json({
+         status: "error",
+         message: err.message,
+      })
+   })
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+   res.status(err.status || 500)
+   res.json({
+      status: "error",
+      message: err.toString(),
+   })
+})
 
 
-module.exports = app;
+module.exports = app
