85ea27cbe691584b0d1444aa9fcae5ebd5dec3fa
Alexander Löhr <alex@alexloehr.net>
2023-09-22 12:41:44 +0200

    converted recentUploads.vue to composition api

--- vue/src/components/recentUploads.vue
+++ vue/src/components/recentUploads.vue
@@ -1,41 +1,39 @@
+<script setup>
+import moment from 'moment'
+import 'moment/locale/de'
+
+moment.locale("de")
+
+const props = defineProps(
+    {
+       uploaded: {
+          type: Array,
+          required: true
+       }
+    },
+)
+
+function getDate (date) {
+   return moment(date).format("L LTS")
+}
+
+</script>
+
+
 <template>
-    <div>
-        <ul id="recent-uploads" class="item-list" v-if="uploaded.length">
-            <li v-for="upload in uploaded">
-                <a :href="upload.url" target="_blank">
-                    {{getDate(upload.date)}}
-                    | {{upload.originalName}}
-                    ({{upload.fileSize}})</a>
-            </li>
-        </ul>
-        <p class="shaded" v-if="!uploaded.length"><em>no recent uploads</em></p>
-    </div>
+   <div>
+      <ul id="recent-uploads" class="item-list" v-if="uploaded.length">
+         <li v-for="upload in uploaded">
+            <a :href="upload.url" target="_blank">
+               {{ getDate(upload.date) }}
+               | {{ upload.originalName }}
+               ({{ upload.fileSize }})</a>
+         </li>
+      </ul>
+      <p class="shaded" v-if="!uploaded.length"><em>no recent uploads</em></p>
+   </div>
 </template>
 
-<script>
-    import moment from 'moment'
-    import 'moment/locale/de'
-
-    moment.locale("de")
-
-    export default {
-        name: "recentUploads",
-        props: ["uploaded"],
-        data() {
-            return {
-                // uploads: [],
-            }
-        },
-        created: function () {
-            // TODO letzte uploads aus localstorage laden
-        },
-        methods: {
-            getDate: function (date) {
-                return moment(date).format("L LTS")
-            }
-        }
-    }
-</script>
 
 <style scoped>
 
