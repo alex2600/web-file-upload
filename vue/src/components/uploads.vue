648e4cf2d224770c6256cd7511198898f3bbf1d9
Alexander Löhr <alex@alexloehr.net>
2023-09-22 12:31:26 +0200

    converted uploads.vue to composition api

--- vue/src/components/uploads.vue
+++ vue/src/components/uploads.vue
@@ -1,36 +1,41 @@
+<script setup>
+import _ from 'lodash'
+import uploadFile from './uploadFile.vue'
+import {watch} from "vue"
+
+const props = defineProps(
+    {
+       files: {
+          type: Array,
+          required: true
+       }
+    },
+)
+
+watch(() => props.files, (val) => {
+   console.log("__FILES changed", val)
+})
+
+function deleteItem (file) {
+   console.log("__DELETE", file)
+   _.pull(this.files, file)
+   this.$forceUpdate()
+}
+
+</script>
+
+
 <template>
-    <div>
-        <ul id="upload-q" class="item-list" v-if="files.length">
-            <li v-for="file in files">
-                <upload-file :file="file" v-on:delete="deleteItem"></upload-file>
-            </li>
-        </ul>
-        <p class="shaded" v-if="!files.length"><em>no running uploads</em></p>
-    </div>
+   <div>
+      <ul id="upload-q" class="item-list" v-if="files.length">
+         <li v-for="file in files">
+            <upload-file :file="file" v-on:delete="deleteItem"></upload-file>
+         </li>
+      </ul>
+      <p class="shaded" v-if="!files.length"><em>no running uploads</em></p>
+   </div>
 </template>
 
-<script>
-    import _ from 'lodash'
-    import uploadFile from './uploadFile.vue'
-
-    export default {
-        name: "uploads",
-        components: {uploadFile},
-        props: ["files"],
-        methods: {
-            deleteItem: function (file) {
-                console.log("__DELETE", file)
-                _.pull(this.files, file)
-                this.$forceUpdate()
-            }
-        },
-       watch: {
-            files: function (val) {
-                console.log("__FILES", val)
-            }
-        }
-    }
-</script>
 
 <style scoped>
 
